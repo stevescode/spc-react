4c4767711fb0ba014fe541dc4ec26b8522bb04e9
stevescode <steves106@hotmail.com>
2023-09-25 14:02:47 +0100

    Added Dark Mode 😼

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+import zones from './config/zones';
+
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './styles/theme';
 
 import { WebsocketProvider } from "./context/WebsocketProvider";
 import { Header } from "./components/Header";
@@ -10,44 +16,32 @@ import { Zone } from "./components/Zone";
 import { Raw } from "./components/Raw";
 import Clock from './components/Clock';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState("light");
+  const isDarkTheme = theme === "dark";
+  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
   return (
-    <>
+  <>  
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>
+          {isDarkTheme ? <span>Light mode</span> : <span>Dark Mode</span>}
+        </button>
+    <WebsocketProvider>
     <Container>
       <Header />
       <Clock />
-      <WebsocketProvider>
         <Raw />
-         <Row>
-          <Col lg={3} md={6}>
-            <Zone description="Upstairs Landing¦ZONE¦2¦FIRST FLOOR" friendlyName="Upstairs Landing" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Front Bedroom¦ZONE¦2¦FIRST FLOOR" friendlyName="Front Bedroom" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Nursery¦ZONE¦2¦FIRST FLOOR" friendlyName="Nursery" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Garage¦ZONE¦1¦GROUND FLOOR" friendlyName="Garage" />
-          </Col>
-        </Row>
         <Row>
-          <Col lg={3} md={6}>
-            <Zone description="Downstairs Hall¦ZONE¦1¦GROUND FLOOR" friendlyName="Downstairs Hall" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Lounge¦ZONE¦1¦GROUND FLOOR" friendlyName="Lounge" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Kitchen Left¦ZONE¦1¦GROUND FLOOR" friendlyName="Kitchen Left" />
-          </Col>
-          <Col lg={3} md={6}>
-            <Zone description="Kitchen Right¦ZONE¦1¦GROUND FLOOR" friendlyName="Kitchen Right" />
-          </Col>
-        </Row>
-      </WebsocketProvider>
+            {zones.map((zone, index) => (
+              <Col lg={3} md={6} key={index}>
+                <Zone description={zone.description} friendlyName={zone.friendlyName} />
+              </Col>
+            ))}
+          </Row>
     </Container>
+    </WebsocketProvider>
+    </ThemeProvider>
     </>
   )
 }
--- src/styles/zonestyles.js
+++ src/styles/zonestyles.js
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyles = createGlobalStyle`
+  body {
+    display: flex;
+    justify-content: center;
+    padding: 1.5em;
+    margin: 0.5em 10em;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.color};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+`;
+export const lightTheme = {
+  backgroundColor: '#121723',
+  color:'white'
+};
+
+export const darkTheme = {
+  backgroundColor: 'white',
+  color:'black'
+};
+
+
 export const ZoneContainer = styled.div`
   display: flex;
   justify-content: center;
