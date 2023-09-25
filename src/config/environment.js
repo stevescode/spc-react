31c9d423113a6415b97872219c492d198d9fed3d
stevescode <steves106@hotmail.com>
2023-09-25 14:00:48 +0100

    Extracting configuration from main code
    Removed config from main code and introduced config/env files

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,14 @@
-// STAGING - points to local websocket server
-// PROD - points to remove websocket server
+const environment = process.env.REACT_APP_ENV || 'STAGING';
 
-export const ENV = "STAGING";
+const config = {
+  STAGING: {
+    WEBSOCKET_IP: '127.0.0.1',
+    WEBSOCKET_PORT: '8999',
+  },
+  PRODUCTION: {
+    WEBSOCKET_IP: '192.168.100.123',
+    WEBSOCKET_PORT: '8088/ws/spc',
+  },
+};
+
+export default config[environment];
---
+++ src/config/zones.js
@@ -0,0 +1,37 @@
+const zones = [
+    {
+      description: 'Upstairs Landing¦ZONE¦2¦FIRST FLOOR',
+      friendlyName: 'Upstairs Landing',
+    },
+    {
+      description: 'Front Bedroom¦ZONE¦2¦FIRST FLOOR',
+      friendlyName: 'Front Bedroom',
+    },
+    {
+      description: 'Nursery¦ZONE¦2¦FIRST FLOOR',
+      friendlyName: 'Nursery',
+    },
+    {
+      description: 'Garage¦ZONE¦1¦GROUND FLOOR',
+      friendlyName: 'Garage',
+    },
+    {
+      description: 'Downstairs Hall¦ZONE¦1¦GROUND FLOOR',
+      friendlyName: 'Downstairs Hall',
+    },
+    {
+      description: 'Lounge¦ZONE¦1¦GROUND FLOOR',
+      friendlyName: 'Lounge',
+    },
+    {
+      description: 'Kitchen Left¦ZONE¦1¦GROUND FLOOR',
+      friendlyName: 'Kitchen Left',
+    },
+    {
+      description: 'Kitchen Right¦ZONE¦1¦GROUND FLOOR',
+      friendlyName: 'Kitchen Right',
+    },
+  ];
+  
+  export default zones;
+  
