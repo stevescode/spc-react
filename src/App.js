72f4c5cc3cdc746cc0ec354c23dfe6e317f28008
stevescode <steves106@hotmail.com>
2023-10-09 21:38:50 +0100

    Migration of main components from App into sub components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import React from 'react';
-import { useState } from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-import zones from './config/zones';
-
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme, GlobalStyles } from './styles/theme';
-
+import { ThemeProvider } from './context/ThemeProvider';
 import { WebsocketProvider } from "./context/WebsocketProvider";
-import { Header } from "./components/Header";
-import { Zone } from "./components/Zone";
-import { Raw } from "./components/Raw";
-import Clock from './components/Clock';
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+console.log(process.env.REACT_APP_ENV);
 
 const App = () => {
-  const [theme, setTheme] = useState("light");
-  const isDarkTheme = theme === "dark";
-  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
   return (
-  <>  
-    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <button onClick={toggleTheme}>
-          {isDarkTheme ? <span>Light mode</span> : <span>Dark Mode</span>}
-        </button>
-    <WebsocketProvider>
-    <Container>
-      <Header />
-      <Clock />
-        <Raw />
-        <Row>
-            {zones.map((zone, index) => (
-              <Col lg={3} md={6} key={index}>
-                <Zone description={zone.description} friendlyName={zone.friendlyName} />
-              </Col>
-            ))}
-          </Row>
-    </Container>
-    </WebsocketProvider>
+    <ThemeProvider>
+      <WebsocketProvider>
+        <Router>
+          <Nav />
+          <Main />
+        </Router>
+      </WebsocketProvider>
     </ThemeProvider>
-    </>
-  )
+  );
 }
 
 export default App;
